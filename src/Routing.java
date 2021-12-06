4a3354dcec8cefb5795df6095dc547471cd8fda4
jmskeoch <jmskeoch@yahoo.com>
2021-12-06 09:11:01 -0800

    Fix insertion and implement search along with blacklist

--- src/Routing.java
+++ src/Routing.java
@@ -8,8 +8,13 @@ public class Routing {
 
     public static void main(String[] args) {
         Routing routes = new Routing();
-        routes.insert("192.2.2.2");
-        routes.insert("192.2.2.5");
+        routes.insert("255.0.0.0");
+        routes.insert("10.8.52.3");
+        routes.insert("10.8.10.8");
+
+        routes.print();
+
+        routes.blacklist("10.8.52.3");
 
         routes.print();
     }
@@ -17,15 +22,16 @@ public class Routing {
     public void insert(String data) { insert(data, root); } // Wrapper method
 
     /**
-     * This method appropriately inserts the binary data as a node into the tree
-     * @param data The ~binary~ data to be inserted into the tree
+     * This method appropriately inserts the number data as a node into the tree
+     * @param data The ~Integer~ data to be inserted into the tree
      */
-    public void insert(String data, Node current) { //TODO Implement this first
-        Node node = current.children[Integer.parseInt(data.substring(0, 1))]; // Set the current node to where we want to insert
+    public void insert(String data, Node current) {
         data = data.replace(".", ""); // Since we are working with IPs, remove all periods
+        Node node = current.children[Integer.parseInt(data.substring(0, 1))]; // Set the current node to where we want to insert
 
         // Begin insertion cases
         if (data.length() == 0) return; // Stop insertion if there is no data to insert
+
         if (node == null) { // If there's no present node for our data, create it
             node = new Node();
             node.data = data;
@@ -55,18 +61,39 @@ public class Routing {
 
             splitNode.data = node.data.substring(idx); // Reset the node's data so there is none repeating
             node.data = node.data.substring(0, idx);
-            node.isIp = false;
 
             splitNode.children = node.children;
             node.children = new Node[10]; // Properly reset both node's children
             node.children[Integer.parseInt(splitNode.data.substring(0, 1))] = splitNode;
 
+            splitNode.isIp = node.isIp;
+            node.isIp = false;
+
             insert(data.substring(idx), node); // Insert the other different data as a new node
         }
+        current.children[Integer.parseInt(data.substring(0, 1))] = node;
+    }
+
+    public boolean search(String data) {
+        return search(data, root);
+    }
+
+    public boolean search(String data, Node current) {
+        if (data == null || data.length() == 0) return false;
+
+        data = data.replace(".", "");
+        Node node = current.children[Integer.parseInt(data.substring(0, 1))];
+        if (node == null) return false;
+        if (node.data.equals(data)) return node.isIp;
+        if (data.startsWith(node.data)) return search(data.substring(node.data.length()), node);
+
+        return false;
     }
 
     // Wrapper method
     public void print() {
+        System.out.println("ALL ACCEPTED IP ADDRESSES");
+        System.out.println("=========================");
         if (root != null) print(root, "");
     }
 
@@ -76,28 +103,30 @@ public class Routing {
      * @param str The recursive portion containing the actual data to print
      */
     public void print(Node current, String str) {
-        if (current.isIp) System.out.println(str);
+        if (current.isIp && !current.blacklisted) System.out.println(str);
 
         for(Node node: current.children) {
             if (node != null) print(node, str + node.data);
         }
     }
 
+    // Wrapper method
+    public void blacklist(String data) {
+        blacklist(data, root);
+    }
     /**
      * This method searches through the tree, inserts if the parameter isn't already existing, and marks the node as blacklisted
-     * @param address The address to blacklist in 00110111001001 form
+     * @param data The address to blacklist in 192.2.2.4 form
+     * @param current Recursive parameter for a radix node
      */
-    public void blacklist(int address) { //TODO implement this after insertion and conversion are completed and functional
+    public void blacklist(String data, Node current) {
+        if (data == null || data.length() == 0) return;
 
-    }
-
-    /**
-     * This method converts a typical ip address into binary form so that each node may only have two children
-     * @param address The address to be converted in 255.0.0.0 form
-     * @return Returns the binary result after converting between something like 255.0.0.0 to 00110111001001
-     */
-    public int ipToBinary(String address) { //TODO Implement this secondly
-        return -1;
+        data = data.replace(".", "");
+        Node node = current.children[Integer.parseInt(data.substring(0, 1))];
+        if (node == null) return;
+        if (node.data.equals(data)) node.blacklisted = true;
+        if (data.startsWith(node.data)) blacklist(data.substring(node.data.length()), node);
     }
 }
 
@@ -112,7 +141,7 @@ class Node {
     Node[] children;
 
     public Node() {
-        this.data = "-1";
+        this.data = "";
         this.isIp = false;
         this.blacklisted = false;
         this.children = new Node[10];
